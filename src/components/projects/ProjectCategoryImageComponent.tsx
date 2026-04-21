61747e7c3e8a5b3a37a0c859d1650fe56736989d
ColdIcyDays <erik.ljungman@gmail.com>
2026-04-22 00:32:28 +0200

    Test for project hover
     - Title.

--- src/components/projects/ProjectCategoryImageComponent.tsx
+++ src/components/projects/ProjectCategoryImageComponent.tsx
@@ -3,7 +3,14 @@ import type {ProjectData} from "../../json-interfaces/projects.tsx";
 export function ProjectCategoryImageComponent({ someCategoryData }: { someCategoryData: ProjectData }) {
     return (
         <div className='max-w-77 max-h-77 shrink-0'>
-            <img src={someCategoryData.ProjectImage}/>
+            <div className='h-full w-full group/projectHovered'>
+                <img className='reg-10-img ring-clr-10-1/50 object-cover w-77 h-77 group-hover/projectHovered:h-55' src={someCategoryData.ProjectImage}/>
+
+                <div className='w-full hidden group-hover/projectHovered:block justify-evenly'>
+                    <button className='std-button w-[96px] self-start'>Details</button>
+                    <button className='std-button w-[96px] self-end'>Link</button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/projects/ProjectCategoryImageScrollContainer.tsx
+++ src/components/projects/ProjectCategoryImageScrollContainer.tsx
@@ -5,7 +5,7 @@ export function ProjectCategoryImageScrollContainer({ someCategoryData } : { som
     /* TODO: Figure out how to caluclate the width correctly*/
     return (
         <div  className='w-full pl-12 pr-12'>
-            <div className='reg-30-div bg-clr-invisible p-12 max-h-full flex flex-row overflow-x-auto w-full min-w-0'>
+            <div className='reg-30-div bg-clr-invisible p-12 max-h-full min-h-[404px] flex flex-row overflow-x-auto w-full min-w-0'>
                 <ul className='flex gap-4'>
                     {someCategoryData.map((data, index) => (
                         <ProjectCategoryImageComponent key={"CategoryImage" + index} someCategoryData={data}/>
