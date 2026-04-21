2a7b6ba3f8669190b9cdfb40ecccdbf9159d2c7c
ColdIcyDays <erik.ljungman@gmail.com>
2026-04-21 17:15:58 +0200

    Added project category component
     - Title.

---
+++ src/components/projects/ProjectCategory.tsx
@@ -0,0 +1,20 @@
+import type {ProjectData} from "../../json-interfaces/projects.tsx";
+import {Link} from "react-router-dom";
+
+/*
+* TODO: Consider making more images appear if there is space, i.e get the projects and get the cover image and add it depending on the space.
+*  The projects should also be ordered by date. So newest projects first to oldest. */
+export function ProjectCategory({ ProjCategoryData }: { ProjCategoryData: ProjectData}) {
+    return (
+        <div className="h-max flex flex-row items-center">
+            <img src={ProjCategoryData.CategoryImage} className='ml-auto reg-10-img'/>
+            <div className='reg-30-div  w-[256px] h-max min-h-85 min-w-lg ml-auto flex flex-col'>
+                <h1 className='text-center p-8 text-3xl'>{ProjCategoryData.CategoryTitle}</h1>
+                <p className='p-8'>{ProjCategoryData.CategoryDescription}</p>
+                <Link className='block m-auto' to={"/Projects/" + ProjCategoryData.CategorySubpageDenotation}>
+                    <button className='std-button'>Learn more</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
