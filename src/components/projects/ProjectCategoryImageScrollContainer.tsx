9cb2eefed27606668df6d9a362a3fc94d769d77e
ColdIcyDays <erik.ljungman@gmail.com>
2026-05-03 13:50:38 +0200

    Fixed project image comp
     - Title.

--- src/components/projects/ProjectCategoryImageScrollContainer.tsx
+++ src/components/projects/ProjectCategoryImageScrollContainer.tsx
@@ -16,7 +16,7 @@ export function ProjectCategoryImageScrollContainer({ someCategoryData, aProject
     });*/
 
     return (
-        <div  className='w-full pl-12 pr-12'>
+        <div  className='w-full xl:pl-12 xl:pr-12 px-2'>
             <div ref={horizontalBar} className='reg-30-div ring-clr-10-1/50 bg-clr-invisible p-12 max-h-full min-h-[404px] flex flex-row overflow-x-auto overflow-y-hidden w-full min-w-0'>
                 <ul className='flex gap-12'>
                     {someCategoryData.toReversed().map((data, index) => (
