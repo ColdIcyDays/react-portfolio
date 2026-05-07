9a07f398ef5c3c176a162c8095d9584992109385
ColdIcyDays <erik.ljungman@gmail.com>
2026-05-07 16:44:11 +0200

    Made homepage blurb be min instead of fixed size of 96rem
     - Title.

--- src/components/home/HomeContainer.tsx
+++ src/components/home/HomeContainer.tsx
@@ -30,9 +30,9 @@ export function HomeContainer() {
                                 <div className="m-auto w-max mt-6 mb-6">
                                     <h2 className="xl:text-[32px] text-[24px]">Games and full-stack developer</h2>
                                 </div>
-                                <div className="w-full h-96 grid col-auto">
+                                <div className="w-full h-min grid col-auto">
 
-                                    <p className="pl-8 pr-8 ">
+                                    <p className="pl-8 pr-8">
                                         My name is Erik Ljungman. I'm a new full-stack developer based in Malmö, Sweden.
                                         <br/> <br/>
                                         My background is in Game development, mainly in C++, but now I'm learning full-stack development at Lexicon.
