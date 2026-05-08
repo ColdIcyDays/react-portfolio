b8fd8b9a5882938b9f6918feb94e78626faba2e0
ColdIcyDays <erik.ljungman@gmail.com>
2026-05-08 13:39:02 +0200

    Added github to details page
     - Title.

--- src/components/projects/details/ProjectDetailsContainer.tsx
+++ src/components/projects/details/ProjectDetailsContainer.tsx
@@ -19,23 +19,27 @@ export function ProjectDetailsContainer({ someProjectData } : { someProjectData:
               </div>
               <div className='grow'>
 
-                  <div className='w-full xl:hidden mb-4 flex flex-row justify-center gap-4'>
-                      <Link to={someProjectData.ProjectWebpage} target={"_blank"} rel='noopener noreferrer'>
-                          <button className='block std-button m-auto mt-12'>Go to project</button>
+                  <div className='w-full xl:hidden mb-4 flex justify-center gap-4 mt-12'>
+                      <Link to={someProjectData.ProjectWebpage} target={"_blank"} rel='noopener noreferrer' style={{marginRight: someProjectData.GithubWebpage.length > 0 ? 0 : "auto"}}>
+                          <button className='block std-button m-auto'>Go to project</button>
                       </Link>
 
-                      {/*<Link to={someProjectData.ProjectWebpage} target={"_blank"} rel='noopener noreferrer'>
-                          <button className='block std-button m-auto mt-12'>Go to Github</button>
-                      </Link>*/}
+                      <Link to={someProjectData.GithubWebpage} target={"_blank"} rel='noopener noreferrer' style={{display: someProjectData.GithubWebpage.length > 0 ? "block" : "none"}}>
+                          <button className='block std-button m-auto'>Go to Github</button>
+                      </Link>
                   </div>
 
                   <div>
                       <img className='reg-10-img w-1/2 aspect-square m-auto xl:mt-12 mb-6' src={"/"+ someProjectData.ProjectFullImage}/>
                   </div>
 
-                  <div className='w-full hidden xl:block'>
-                      <Link to={someProjectData.ProjectWebpage} target={"_blank"} rel='noopener noreferrer'>
-                        <button className='block std-button m-auto mb-12'>Go to project</button>
+                  <div className='w-full hidden xl:flex gap-4'>
+                      <Link to={someProjectData.ProjectWebpage} target={"_blank"} rel='noopener noreferrer' className='ml-auto' style={{marginRight: someProjectData.GithubWebpage.length > 0 ? 0 : "auto"}}>
+                        <button className='block std-button '>Go to project</button>
+                      </Link>
+
+                      <Link to={someProjectData.GithubWebpage} target={"_blank"} rel='noopener noreferrer' className='mr-auto' style={{display: someProjectData.GithubWebpage.length > 0 ? "block" : "none"}}>
+                          <button className='block std-button'>Go to Github</button>
                       </Link>
                   </div>
               </div>
